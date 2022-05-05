50534accd25873a0d367afe0b1bb4849edc183b0
LVDVGR <104921872+LVDVGR@users.noreply.github.com>
2022-05-05 22:49:08 +0300

    Update command "Hello"

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import discord
 from discord.ext import commands
 from config import settings
+from hello import hello_phrases
+import random
 
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.command()
 async def hello(ctx):
     author = ctx.message.author
-    await ctx.send(f'Привет, {author.mention}!')
+    hello_index = random.randrange(0, 14)
+    hi = hello_phrases[hello_index]
+    await ctx.send(f'{hi}, {author.mention}!')
 
 bot.run(settings['token'])
+
