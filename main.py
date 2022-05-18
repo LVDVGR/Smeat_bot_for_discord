0450de0ec9db91f1257aa2a2e659a834270f65d4
LVDVGR <104921872+LVDVGR@users.noreply.github.com>
2022-05-18 18:44:14 +0300

    Add command "leave"

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
+import audioop
+
 import discord
 from discord.ext import commands
 from config import settings
 from hello import hello_phrases
 import random
 from youtube_dl import YoutubeDL
+import time
 
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 YDL_OPTIONS = {'format': 'worstaudio/best', 'noplaylist': 'False', 'simulate': 'True',
                'preferredquality': '192', 'preferredcodec': 'mp3', 'key': 'FFmpegExtractAudio'}
 bot = commands.Bot(command_prefix = settings['prefix'])
+
+server = None
+
+def recharge(music):
+    music = False
+    return music
 
 @bot.command()
 async def hello(ctx):
@@ -24,6 +33,7 @@ async def hello(ctx):
 @bot.command()
 async def play(ctx, url):
     vc = await ctx.message.author.voice.channel.connect()
+    voice = discord.utils.get(bot.voice_clients, guild=server)
 
     with YoutubeDL(YDL_OPTIONS) as ydl:
         if 'https://' in url:
@@ -32,8 +42,23 @@ async def play(ctx, url):
             info = ydl.extract_info(f'ytsearch: {url} ', download=False)['entrise'][0]
 
     url = info['formats'][0]['url']
-
     vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=url, **FFMPEG_OPTIONS))
+
+
+
+@bot.command()
+async def leave(ctx):
+    global server
+    server = ctx.guild
+    voice = discord.utils.get(bot.voice_clients, guild = server)
+
+    if voice is None:
+        await ctx.channel.send(f'{ctx.author.mention}, меня тут и не было, долбаёб!')
+    else:
+        await voice.disconnect()
+
+
+
 
 
 
