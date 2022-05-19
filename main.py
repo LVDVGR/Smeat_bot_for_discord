557f3fe323b571af9f7eca6e5375436bbb221611
LVDVGR <104921872+LVDVGR@users.noreply.github.com>
2022-05-19 21:45:37 +0300

    troubleshoot layout issues and update "hello"

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import discord
 from discord.ext import commands
 from config import settings
-from hello import hello_phrases
+from hello import hello_phrases, shift_alt_phrase
 import random
 from youtube_dl import YoutubeDL
 import time
@@ -15,7 +15,11 @@ YDL_OPTIONS = {'format': 'worstaudio/best', 'noplaylist': 'False', 'simulate': '
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 server = None
+music_list = []
 
+
+
+#-------------------------------------------------------------hello------------------------------------
 @bot.command()
 async def hello(ctx):
     author = ctx.message.author
@@ -27,22 +31,51 @@ async def hello(ctx):
     hi = hello_phrases[hello_index]
     await ctx.send(f'{hi}, {author.mention}!')
 
+
+@bot.command()
+async def руддщ(ctx):
+    author = ctx.message.author
+    check = ctx.message.content
+    s = str(check)
+    s = check.replace('Smeat ', '', 1)
+    print(s)
+    hello_index = random.randrange(0, 14)
+    hi = hello_phrases[hello_index]
+    await ctx.send(f'{hi}, {author.mention}!')
+    shiftAlt_index = random.randrange(0, len(shift_alt_phrase) - 1)
+    shift = shift_alt_phrase[shiftAlt_index]
+    await ctx.send(shift)
+#----------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+#-----------------------------------------------------------play---------------------------------------
 @bot.command()
 async def play(ctx, url):
     vc = await ctx.message.author.voice.channel.connect()
     voice = discord.utils.get(bot.voice_clients, guild=server)
 
     with YoutubeDL(YDL_OPTIONS) as ydl:
+
         if 'https://' in url:
             info = ydl.extract_info(url, download=False)
         else:
             info = ydl.extract_info(f'ytsearch: {url} ', download=False)['entrise'][0]
 
     url = info['formats'][0]['url']
+    #if(voice.is_playing):
+     #   music_list.append()
     vc.play(discord.FFmpegPCMAudio(executable="ffmpeg\\ffmpeg.exe", source=url, **FFMPEG_OPTIONS))
+#----------------------------------------------------------------------------------------------------------
 
 
 
+
+
+#---------------------------------------------------leave-----------------------------------------------------
 @bot.command()
 async def leave(ctx):
     global server
@@ -53,7 +86,12 @@ async def leave(ctx):
         await ctx.channel.send(f'{ctx.author.mention}, меня тут и не было!')
     else:
         await voice.disconnect()
+#----------------------------------------------------------------------------------------------------------
 
+
+
+
+#-------------------------------------------------stop---------------------------------------------------
 @bot.command()
 async def stop(ctx):
     global server
@@ -64,8 +102,12 @@ async def stop(ctx):
         await voice.pause()
     else:
         await ctx.channel.send(f'{ctx.author.mention}, уже остановлено.')
+#----------------------------------------------------------------------------------------------------------
 
 
+
+
+#-------------------------------------------------------------resume------------------------------------
 @bot.command()
 async def resume(ctx):
     global server
@@ -77,8 +119,22 @@ async def resume(ctx):
     else:
         await ctx.channel.send(f'{ctx.author.mention}, уже остановлено.')
 
+async def куыгьу(ctx):
+    global server
+    server = ctx.guild
+    voice = discord.utils.get(bot.voice_clients, guild = server)
+
+    if voice.is_paused():
+        await voice.resume()
+    else:
+        await ctx.channel.send(f'{ctx.author.mention}, уже остановлено.')
+#----------------------------------------------------------------------------------------------------------
 
 
+
+
+
+#-------------------------------------------------------volume--------------------------------------------
 @bot.command()
 async def volume(ctx, vol):
     voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
@@ -93,8 +149,10 @@ async def volume(ctx, vol):
             await ctx.send("Недопустимый диапазон значений.")
     else:
         await ctx.send("Я не нахожусь в войс канале.")
+#----------------------------------------------------------------------------------------------------------
 
 
+#----------------------------------------------show---------------------------------------------------
 @bot.command()
 async def show(ctx, search):
     new_search = str(search)
@@ -105,6 +163,10 @@ async def show(ctx, search):
     embed = discord.Embed(color = 0xff9900, title = f'Random {new_search}')
     embed.set_image(url = json_data['link'])
     await ctx.send(embed = embed)
+#----------------------------------------------------------------------------------------------------------
+
+
+
 
 
 bot.run(settings['token'])
