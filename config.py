7f9d1a5240933fa4b62d776357942087c06200c3
LVDVGR <104921872+LVDVGR@users.noreply.github.com>
2023-01-11 19:26:41 +0300

    add custom playlist

---
+++ config.py
@@ -0,0 +1,9 @@
+settings = {'token': 'OTcxNDU0MjEyOTQ2OTIzNTYx.GFY1IV.Whr8zLX0Vzumr92YuqcOXwTRr3e7hvMU92DgmQ',
+            'prefix': ['smeat ', 'Mr.Smeat ', 'S ', 's ', 'Ыьуфе ', 'ыьуфе ', 'Ы ', 'ы ', 'Смит ', 'Смит, ', 'смит ', 'смит, '], #можно сделать перечень префиксов при подаче листа строк
+            'ffmpeg_path': 'G:\\ffmpeg-master-latest-win64-gpl-shared_for_bot\\ffmpeg-master-latest-win64-gpl-shared\\bin\\ffmpeg.exe'}
+
+
+creds = {"host"     : "127.0.0.1",
+         "user"     : "postgres",
+         "password" : "zazaza3245",
+         "db_name"  : "smeat_playlists"}
